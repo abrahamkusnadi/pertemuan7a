bb167a9ba1c7f19ecc9595e3898c70a76c530508
Ignatius Abraham Aristio Kusnadi <abrahamkusnadi@gmail.com>
2026-02-23 21:07:01 +0700

    Fixed error delete operation

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::get('/', function () {
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books', [BookController::class, 'store'])->name('books.store');
-Route::post('/books/delete/{$id}', [BookController::class, 'destroy'])->name('books.delete');
+Route::post('/books/delete/{id}', [BookController::class, 'destroy'])->name('books.delete');
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
 Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::post('/categories/delete/{$id}', [CategoryController::class, 'destroy'])->name('categories.delete');
+Route::post('/categories/delete/{id}', [CategoryController::class, 'destroy'])->name('categories.delete');
